366afec57c89b720fea2cd308130cf7832bc6acc
Sangin Han <hugedreamhd@gmail.com>
2024-07-13 16:59:31 +0900

    feat: 240712 interface에 Result 패턴 추가 && enum 선언

--- project/learn_flutter_togerther/lib/03_food_recipe_app/repository/recipe_repository.dart
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/repository/recipe_repository.dart
@@ -1,5 +1,10 @@
+import '../core/result.dart';
 import '../model/recipe.dart';
 
 abstract interface class RecipeRepository {
-  Future<List<Recipe>> fetchFood();
+  Future<Result<List<Recipe>, RecipeRepositoryError>> fetchRecipe();
+}
+//에러처리
+enum RecipeRepositoryError {
+  networkError,
 }
