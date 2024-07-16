e9dbbbc152ae4458ed46e63c2b2359150b067ccc
Sangin Han <hugedreamhd@gmail.com>
2024-07-17 01:42:53 +0900

    fix: main home파라미터 수정

--- project/learn_flutter_togerther/lib/03_food_recipe_app/main.dart
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_togerther/03_food_recipe_app/data/mock_recipe_data_source.dart';
+import 'package:learn_flutter_togerther/03_food_recipe_app/repository/recipe_repository_impl.dart';
 
-import 'presentation/ui/home_screen.dart';
+import 'presentation/home/home_screen.dart';
 
 class RecipeApp extends StatelessWidget {
   const RecipeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: HomeScreen(),
-        ),
-      ),
+    return MaterialApp(
+      home: HomeScreen(
+          repository: RecipeRepositoryImpl(
+        recipeDataSource: MockRecipeDataSource(),
+      )),
     );
   }
 }
