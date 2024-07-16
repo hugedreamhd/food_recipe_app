98b4b60e5660063bee579851495b028e6e668583
Sangin Han <hugedreamhd@gmail.com>
2024-07-17 00:32:29 +0900

    fix: result ver1 으로 변경

--- project/learn_flutter_togerther/lib/03_food_recipe_app/core/result.dart
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/core/result.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'result.freezed.dart';
 
 @freezed
-sealed class Result<T, E> with _$Result<T, E> {
+sealed class Result<T> with _$Result<T> {
   const factory Result.success(T data) = Success;
   const factory Result.error(String e) = Error;
 }
