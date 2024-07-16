b11a460337d9c1ac4725b72e156ee51db3ff0c9c
Sangin Han <hugedreamhd@gmail.com>
2024-07-17 00:31:02 +0900

    fix: RecipeRepositoryImpl  Result 패턴 ver1으로 수정

--- project/learn_flutter_togerther/lib/03_food_recipe_app/repository/recipe_repository.dart
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/repository/recipe_repository.dart
@@ -2,9 +2,5 @@ import '../core/result.dart';
 import '../model/recipe.dart';
 
 abstract interface class RecipeRepository {
-  Future<Result<List<Recipe>, RecipeRepositoryError>> fetchRecipe();
-}
-//에러처리
-enum RecipeRepositoryError {
-  networkError,
+  Future<Result<List<Recipe>>> fetchRecipe();
 }
--- project/learn_flutter_togerther/lib/03_food_recipe_app/repository/recipe_repository_impl.dart
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/repository/recipe_repository_impl.dart
@@ -1,21 +1,28 @@
+import 'package:flutter/widgets.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/core/result.dart';
+import 'package:learn_flutter_togerther/03_food_recipe_app/data/recipe_data_source.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/model/recipe.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/repository/recipe_repository.dart';
 
 class RecipeRepositoryImpl implements RecipeRepository {
+  final RecipeDataSource _recipeDataSource;
+
+  RecipeRepositoryImpl({
+    required RecipeDataSource recipeDataSource,
+  }) : _recipeDataSource = recipeDataSource;
+
   @override
-  Future<Result<List<Recipe>, RecipeRepositoryError>> fetchRecipe() async {
-    final List<Recipe> recipes =[
-      Recipe(
-        foodTitle: 'Traditional spare ribs baked',
-        creator: 'By Chef John',
-        time: 20,
-        rating: 4.0,
-        imagePath:
-            'https://s3-alpha-sig.figma.com/img/2234/134e/6e53ef9148ab9085bbd1369e270f0bba?Expires=1721606400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WvmODdewmuqyvaoW6smiu71~z7wemAUUHfSRIdzVffzbPFGK36dRxrIBPnxgx8A~WYdpRx1t9QhbL-9wTJJD~l0TSge5mUABr07Ly0B~Qr30LMN2XW2yIbuH6oDgk3-BDY3D0JKlvfJC4~XzxKf4y399b~M0la-1mMnm5Yn1bHFE6CPjRrV8-HeDhnA4jSX5b13XrFT7OGj3nw3mtKtpAfsYDRyPDdEZwSpCCOEs6BVpjl3R5VzinWhwCBDEFBKIVmHlJfhxzYROHVcR9FKqxY8SOiV2AxI~SwkXXILtO7kRWKxhM2ioRdFkgHde7LmfrS8C-DgZ2PDTrr5RsL~3dQ__',
-      ),
-    ];
-    return Result.success(recipes);
+  Future<Result<List<Recipe>>> fetchRecipe() async {
+    try {
+      final result = await _recipeDataSource.getRecipes();
+      return Result.success(result);
+    } catch (e) {
+      return const Result.error('fail load');
+    }
+
   }
 
+
 }
+
+
