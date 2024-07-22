2cb54ad9e6f5894de53410945cd20c456ca5e8d4
Sangin Han <hugedreamhd@gmail.com>
2024-07-22 21:28:31 +0900

    fix: route 기본 로케이션 수정

--- project/learn_flutter_togerther/lib/03_food_recipe_app/core/routes.dart
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/core/routes.dart
@@ -1,44 +1,34 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/data/mock_recipe_data_source.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/model/recipe.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/presentation/home/home_screen.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/presentation/saved_recipe_detail_view/saved_recipe_detail_view.dart';
+import 'package:learn_flutter_togerther/03_food_recipe_app/presentation/sing_up/sign_up_screen.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/repository/recipe_repository_impl.dart';
-
-final recipeDataSource = MockRecipeDataSource();
-final repository = RecipeRepositoryImpl(
-  recipeDataSource: recipeDataSource,
-);
 
 final router = GoRouter(
   initialLocation: '/',
-  routes: [
+  routes: <RouteBase>[
     GoRoute(
-      path: '/home_screen',
+      path: '/',
       builder: (context, state) => HomeScreen(
         repository: RecipeRepositoryImpl(
           recipeDataSource: MockRecipeDataSource(),
         ),
       ),
     ),
-    // GoRoute(
-    //   path: '/sign_up',
-    //   builder: (context, state) => const SignUpScreen(),
-    // ),
     GoRoute(
-        path: '/saved_recipe_detail_view',
-        builder: (context, state) {
-          final recipe = state.extra as Recipe;
-          return SavedRecipeDetailView(
-            recipe: recipe,
-          );
-        }),
-    // GoRoute(
-    //   path: '/splash_screen',
-    //   builder: (context, state) {
-    //     return const SplashScreen();
-    //   },
-    // ),
+      path: '/sign_up',
+      builder: (context, state) => const SignUpScreen(),
+    ),
+    GoRoute(
+      path: '/saved_recipe_detail_view',
+      builder: (context, state) {
+        final recipe = state.extra as Recipe;
+        return SavedRecipeDetailView(
+          recipe: recipe,
+        );
+      },
+    ),
   ],
 );
