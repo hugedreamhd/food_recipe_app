163af5cb6d0cfbca80fbba77fb679ede78b81000
Sangin Han <hugedreamhd@gmail.com>
2024-07-22 21:30:23 +0900

    fix: main 파일 router 수정

--- project/learn_flutter_togerther/lib/03_food_recipe_app/main.dart
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter_togerther/03_food_recipe_app/core/routes.dart';
 
+void main() {
+  runApp(
+    const RecipeApp(),
+  );
+}
+
 class RecipeApp extends StatelessWidget {
-  const RecipeApp({super.key});
+  const RecipeApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +18,4 @@ class RecipeApp extends StatelessWidget {
       routerConfig: router,
     );
   }
-}
-
-void main() {
-  runApp(const RecipeApp());
 }
