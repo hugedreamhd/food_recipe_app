42fbb3f8017a1b67af14260d78bbab72bc740a73
Sangin Han <hugedreamhd@gmail.com>
2024-07-23 14:51:46 +0900

    fix: model을 3 food_recipe_app번 폴더로 복사

---
+++ project/learn_flutter_togerther/lib/03_food_recipe_app/model/food_recipe.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'food_recipe.freezed.dart';
+
+part 'food_recipe.g.dart';
+
+@freezed
+class FoodRecipe with _$FoodRecipe {
+  const factory FoodRecipe({
+    required String foodTitle,
+    required String creator,
+    required String time,
+    required double rating,
+    required String imagePath,
+  }) = _FoodRecipe;
+
+  factory FoodRecipe.fromJson(Map<String, Object?> json) => _$FoodRecipeFromJson(json);
+}
