f61f1b010146d6e9ef430346a76229f44698a75d
Sangin Han <hugedreamhd@gmail.com>
2024-07-31 16:17:00 +0900

    feat: textfield readonly 적용

--- lib/03_food_recipe_app/presentation/component/search_input_layout.dart
+++ lib/03_food_recipe_app/presentation/component/search_input_layout.dart
@@ -3,57 +3,56 @@ import 'package:flutter/material.dart';
 import '../ui/color_styles.dart';
 import '../ui/text_styles.dart';
 
-class SearchInputLayout extends StatefulWidget {
+class SearchInputLayout extends StatelessWidget {
   final String inputSearch;
+  final bool isCursor;
 
   const SearchInputLayout({
     super.key,
     required this.inputSearch,
+    this.isCursor = true,
   });
 
   @override
-  State<SearchInputLayout> createState() => _SearchInputLayoutState();
-}
-
-class _SearchInputLayoutState extends State<SearchInputLayout> {
-  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Container(
-            height: 40,
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: ColorStyles.gray4),
-            ),
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 16,
-                  width: 16,
-                  child: Image.asset('asset/images/search-normal.png'),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: widget.inputSearch,
-                      hintStyle: TextStyles.smallerTextRegular.copyWith(
-                        color: ColorStyles.gray4,
-                      ),
-                      border: InputBorder.none,
-                      contentPadding: const EdgeInsets.symmetric(vertical: 15),
-                    ),
-
+    return SingleChildScrollView(
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              height: 40,
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: ColorStyles.gray4),
+              ),
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 16,
+                    width: 16,
+                    child: Image.asset('asset/images/search-normal.png'),
                   ),
-                ),
-              ],
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: TextField(
+                      readOnly: isCursor,
+                      decoration: InputDecoration(
+                        hintText: inputSearch,
+                        hintStyle: TextStyles.smallerTextRegular.copyWith(
+                          color: ColorStyles.gray4,
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
