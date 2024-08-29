0ea9b293989908cd727af76ff06b93fb4b2c12ad
Sangin Han <hugedreamhd@gmail.com>
2024-08-30 00:28:32 +0900

    feat: saved recipe viewModel 작성

---
+++ lib/03_food_recipe_app/presentation/saved_recipe/saved_recipe_view_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:food_recipe_app/03_food_recipe_app/core/result.dart';
+import 'package:food_recipe_app/03_food_recipe_app/model/recipe.dart';
+import 'package:food_recipe_app/03_food_recipe_app/repository/recipe_repository.dart';
+
+class SavedRecipeViewModel with ChangeNotifier {
+  final SavedRecipeRepository _recipeRepository;
+
+  SavedRecipeViewModel(this._recipeRepository);
+
+  List<Recipe> _recipes = [];
+
+  List<Recipe> get recipes => List.unmodifiable(_recipes);
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  void fetchRecipe() async {
+    _isLoading = true;
+    notifyListeners();
+
+    final result = await _recipeRepository.fetchRecipe();
+    switch (result) {
+      case Success<List<Recipe>>():
+        _recipes = result.data;
+        _isLoading = false;
+        notifyListeners();
+      case Error<List<Recipe>>():
+        print('알수 없는 오류');
+    }
+  }
+}
